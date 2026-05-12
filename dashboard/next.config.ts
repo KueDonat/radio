346afb1874d71ad2181a0be48af92d01d370166b
KueDonat <76199336+KueDonat@users.noreply.github.com>
2026-05-12 11:23:50 +0700

    Cleanup next.config.ts

--- dashboard/next.config.ts
+++ dashboard/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["pg", "@prisma/client"],
+  serverExternalPackages: ["pg"],
 };
 
 export default nextConfig;
